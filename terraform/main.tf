35f3d4681b2898a2098663b21d838ef64d823041
Amin Chouaibi el Azaar <achouaibi@plainconcepts.com>
2021-10-14 21:23:05 +0200

    Base tf code

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,6 @@ terraform {
     storage_account_name = "tfbackend024356e"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
-    access_key           = "VmuhIL9+y8I5QBUJ3GePzBlIFi324ObfgiY/67mXO/9slfv4ySX4wpfs7Q1l+zLR1eCHsl+kL4k5CeMkIMqJzg=="
   }
 }
 
@@ -42,14 +41,6 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   }
 }
 
-resource "azurerm_application_insights" "insights" {
-    name                = var.insights-name
-    location            = "${azurerm_resource_group.rg.location}"
-    resource_group_name = "${azurerm_resource_group.rg.name}"
-    application_type    = "web"
-    retention_in_days   = 730
-}
-
 # Create an Azure Web App for Containers in that App Service Plan
 resource "azurerm_app_service" "webapp" {
   name                = var.web_app_name
@@ -61,7 +52,6 @@ resource "azurerm_app_service" "webapp" {
     DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr.login_server}"
     DOCKER_REGISTRY_SERVER_USERNAME = "${azurerm_container_registry.acr.admin_username}"
     DOCKER_REGISTRY_SERVER_PASSWORD = "${azurerm_container_registry.acr.admin_password}"
-    APPINSIGHTS_INSTRUMENTATIONKEY =  "${azurerm_application_insights.insights.instrumentation_key}"
   }
   site_config {
     linux_fx_version = "DOCKER|${var.container_registry_name}:${var.tag_name}"
@@ -72,24 +62,6 @@ resource "azurerm_app_service" "webapp" {
     type = "SystemAssigned"
   }
 }
-
-resource "azurerm_app_service_slot" "dev" {
-    name                = "${azurerm_app_service.webapp.name}-dev-webapp"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-    app_service_name    = azurerm_app_service.webapp.name
-}
-
-resource "azurerm_app_service_slot" "stg" {
-    name                = "${azurerm_app_service.webapp.name}-stg-webapp"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-    app_service_name    = azurerm_app_service.webapp.name
-}
-
-
 
 output "app_service_name" {
   value = "${azurerm_app_service.webapp.name}"
