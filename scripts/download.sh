3baa2c34fd825c98ad0657f2cf7f58640e15a142
Amin Chouaibi el Azaar <achouaibi@plainconcepts.com>
2021-10-14 20:09:06 +0200

    Deleting files not related to master

--- scripts/download.sh
+++
@@ -1,5 +0,0 @@
- #!/bin/bash
-url=https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_Comunicacion_20210301.ods
-date=$(date +%m-%d-%Y)
-dirName="$date.ods"
-curl $url > $dirName
