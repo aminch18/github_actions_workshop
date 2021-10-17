03f3eb728232a536311794c066787a39df865441
Amin Chouaibi el Azaar <achouaibi@plainconcepts.com>
2021-10-17 14:24:51 +0200

    Pruebas

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-github-actions-ws"
+    storage_account_name = "tfbackend024356e"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
